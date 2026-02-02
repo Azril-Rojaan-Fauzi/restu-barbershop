e823204dfcc40b26c218a6e30ff008e264b0dcb1
Azril Rojaan <endogcity@gmail.com>
2026-02-01 21:51:42 -0600

    feat: membuat halaman about

---
+++ src/components/AboutContent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AboutContent = () => {
+  return (
+    <div className="relative z-10 mx-auto flex h-full max-w-7xl items-center justify-center px-6">
+      <div className="flex flex-col items-center justify-center gap-10 text-center">
+        <h1 className="text-5xl font-bold text-white">About Us</h1>
+        <p className="text-[10px]/5 md:text-sm/7">
+          Didirikan sejak 2014, Pangkas Rambut Restu merupakan barbershop yang
+          dikenal akan kualitas, ketelitian, dan pelayanan profesional. Dengan
+          pengalaman lebih dari 10 tahun, kami melayani berbagai kalangan dengan
+          beragam gaya—mulai dari klasik hingga modern. Kami percaya potongan
+          rambut adalah bagian dari gaya hidup, поэтому kami selalu menjaga
+          kenyamanan, kebersihan, dan hasil terbaik dengan suasana santai serta
+          harga yang bersahabat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutContent;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import AboutContent from "../components/AboutContent";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div
+      className="relative h-screen w-full bg-cover bg-center"
+      style={{
+        backgroundImage: "url(/assets/img/about-bg.jpg)",
+      }}
+    >
+      {/* Gradient */}
+      <div className="pointer-events-none absolute inset-0 bg-linear-to-t from-blue-900 from-0% via-blue-950 via-40% to-transparent to-80% md:via-50%" />
+      <AboutContent />
+    </div>
+  );
 };
 
 export default About;
